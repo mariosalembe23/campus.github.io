027644e12d5fa85da072ce4785f2f55f76eae66e
Mário Lino Salembe <linomario199010@gmail.com>
2023-11-29 10:09:04 -0500

    Adicionado Navegação

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./assets/js/*.js"],
+  theme: {
+    screens: {
+      "small": { "max": "599px" },
+      // @media screen (max-width: 599px) { @content }
+
+      "retrato-tablet": "600px",
+      // @media screen (min-width: 600px) { @content }
+
+      "paisagem-tablet": "900px",
+      // @media screen (min-width: 900px) { @content }
+
+      "desktop": "1200px",
+      // @media screen (min-width: 1200px) { @content }
+
+      "large": "1800px",
+      // @media screen (min-width: 1800px) { @content }
+    },
+    extend: {},
+  },
+  plugins: [],
+};

82b6e480a808e64ede3f2d57210ac971aba375f0
Mário Lino Salembe <linomario199010@gmail.com>
2023-12-01 01:36:25 -0500

    Adicionado Links Finais

---
+++ README.md
@@ -0,0 +1,18 @@
+#Campus - Campismo e Descoberta
+
+**Campus** é um Projecto Front-end, um Site de Campismo para Novas Aventuras e Descobertas incríveis para Estimular a mente e 
+Entreter as Pessoas!
+
+### Tecnologias Usadas
+- HTML, CSS
+- JS
+- TAILWIND CSS
+
+### Imagens
+![](screen.png)
+![](screen2.png)
+![](screen3.png)
+![](screen4.png)
+
+### Links
+[Instagram](https://www.instagram.com/mariosalembe22) | [Linkedim](https://www.linkedin.com/in/m%C3%A1rio-salembe-093797246/) | [URL LIVE]()
--- assets/js/main.js
+++ assets/js/main.js
@@ -15,24 +15,63 @@ document.addEventListener("scroll", scrollingPage);
 
 function showBoxShare() {
   const backgroundMask = document.querySelector(".backgroundMask");
-  const share_page = document.querySelector(".share_page");
+  const shareBox = document.querySelector(".share_box");
   backgroundMask.classList.remove("hidden");
   backgroundMask.classList.add("animatedBackMask");
   backgroundMask.classList.add("flex");
-  share_page.classList.remove("closeBoxShare")
-  share_page.classList.add("showBoxShare");
+  shareBox.classList.remove("closeBoxShare");
+  shareBox.classList.add("showBoxShare");
   document.body.style.overflow = "hidden";
 }
 
 function closeBoxShare() {
   const backgroundMask = document.querySelector(".backgroundMask");
-  const share_page = document.querySelector(".share_page");
+  const shareBox = document.querySelector(".share_box");
 
   setTimeout(() => {
     backgroundMask.classList.add("hidden");
     backgroundMask.classList.remove("flex");
   }, 700);
-  share_page.classList.remove("showBoxShare");
-  share_page.classList.add("closeBoxShare");
+  shareBox.classList.remove("showBoxShare");
+  shareBox.classList.add("closeBoxShare");
   document.body.style.overflow = "auto";
+}
+
+function OpenOffCanvas() {
+  const offcanvasBack = document.querySelector(".offcanvasBack");
+  const offCanvas = document.querySelector(".offcanvas");
+  offcanvasBack.classList.remove("hidden");
+  offcanvasBack.classList.add("animatedBackMask");
+  offCanvas.classList.remove("HideOffCanvas");
+  offCanvas.classList.add("showOffCanvas");
+  document.body.style.overflow = "hidden";
+}
+
+const allLinksOffCanvas = document.querySelectorAll(".link_offcanvas");
+allLinksOffCanvas.forEach((link) => {
+  link.addEventListener("click", closeOffCanvas);
+});
+
+function closeOffCanvas() {
+  const offcanvasBack = document.querySelector(".offcanvasBack");
+  const offCanvas = document.querySelector(".offcanvas");
+  setTimeout(() => {
+    offcanvasBack.classList.add("hidden");
+  }, 700);
+
+  offCanvas.classList.remove("showOffCanvas");
+  offCanvas.classList.add("HideOffCanvas");
+  document.body.style.overflow = "auto";
+}
+
+function copiarTexto() {
+  const texto = document.getElementById("link_share").innerText;
+
+  navigator.clipboard.writeText(texto)
+    .then(function() {
+      alert('Texto copiado com sucesso!');
+    })
+    .catch(function(err) {
+      console.error('Erro ao copiar texto: ', err);
+    });
 }

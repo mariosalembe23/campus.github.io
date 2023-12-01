cbf4676fb46ecc29d51d87a8fbd56ea7e3406449
Mário Lino Salembe <linomario199010@gmail.com>
2023-11-30 21:23:36 -0500

    Adicionado Caixa de Compartilhamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,27 @@ const scrollingPage = () => {
 };
 
 document.addEventListener("scroll", scrollingPage);
+
+function showBoxShare() {
+  const backgroundMask = document.querySelector(".backgroundMask");
+  const share_page = document.querySelector(".share_page");
+  backgroundMask.classList.remove("hidden");
+  backgroundMask.classList.add("animatedBackMask");
+  backgroundMask.classList.add("flex");
+  share_page.classList.remove("closeBoxShare")
+  share_page.classList.add("showBoxShare");
+  document.body.style.overflow = "hidden";
+}
+
+function closeBoxShare() {
+  const backgroundMask = document.querySelector(".backgroundMask");
+  const share_page = document.querySelector(".share_page");
+
+  setTimeout(() => {
+    backgroundMask.classList.add("hidden");
+    backgroundMask.classList.remove("flex");
+  }, 700);
+  share_page.classList.remove("showBoxShare");
+  share_page.classList.add("closeBoxShare");
+  document.body.style.overflow = "auto";
+}
